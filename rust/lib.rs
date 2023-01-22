3adb6a4d4a7defc1c7d7cd3aa504cf1d1d322b81
Matheus T. dos Santos <matheus.santos@edge.ufal.br>
2023-01-22 19:22:00 -0300

    Add c and rust files and moving bridge to its folder
    Signed-off-by: Matheus T. dos Santos <matheus.santos@edge.ufal.br>

---
+++ rust/lib.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_builtins]
+
+extern crate panic_halt;
+extern crate hat;
+extern crate hat_macros;
+extern crate alloc;
+
+use core::time::Duration;
+use hat::*;
+
+#[hat_macros::entry]
+pub async fn main_task() -> TaskResult {
+    loop {
+        log!("Hello, World!");
+        delay(Duration::from_secs(1)).await;
+    }
+
+}
